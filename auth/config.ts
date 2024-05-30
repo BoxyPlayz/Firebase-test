4770148731070c87694b7fb5fe8c2fc6b004b1a3
Boxy <nozakisola@gmail.com>
2024-05-30 11:14:22 -0500

    Update config.ts

--- auth/config.ts
+++ auth/config.ts
@@ -1,12 +1,10 @@
-export const firebaseConfig = {
-  // Your web app's Firebase configuration here
-  // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
-  apiKey: 'API_KEY',
-  authDomain: 'PROJECT_ID.firebaseapp.com',
-  databaseURL: 'https://PROJECT_ID.firebaseio.com',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'PROJECT_ID.appspot.com',
-  messagingSenderId: 'SENDER_ID',
-  appId: 'APP_ID',
-  measurementId: 'G-MEASUREMENT_ID',
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXtBig5mN1tRHIYFnpAT6UsZEYrzroQ7E",
+  authDomain: "skibidithing123.firebaseapp.com",
+  projectId: "skibidithing123",
+  storageBucket: "skibidithing123.appspot.com",
+  messagingSenderId: "61103427980",
+  appId: "1:61103427980:web:2cdbc87074fe26a97246a9",
+  measurementId: "G-CR1QYMBWZL"
 };
